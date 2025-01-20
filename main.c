8b1934011d504e4ad28da973f842674d2f3bf25f
Mesozoic Egg <mesozoic.egg@proton.mail>
2025-01-21 05:34:51 +0800

    wip

--- main.c
+++ main.c
@@ -8,10 +8,9 @@ int main() {
     uint16_t data1[1] = {17948};  // 17948 = 0x461c = 9984 in bf16  0x461c0000 in float32 is 9984
     printf("Input: %x\n", data1[0]);
 
-    // Call the LLVM compiled function
     E_(data0, data1);
 
-    // Print the result
+    // Expected result is 0x70e0, which is integer 9984 represented in float16
     printf("Result: %x\n", data0[0]);
 
     return 0;
